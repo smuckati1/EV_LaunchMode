dc737e8c40ef4cf36c040cde1ef0d27c32bdbc60
sameerkariappa <smuckati@mathworks.com>
2026-01-12 16:56:57 -0800

    Removing callas to matlab.desktop.editor.getAll to prevent warnings and exit errors.

--- Utilities/helperFcns_Project/clear_env.m
+++ Utilities/helperFcns_Project/clear_env.m
@@ -1,22 +1,42 @@
 function clear_env()
 
-closeNoPrompt(matlab.desktop.editor.getAll);
+% disp('Closing all desktop editors matlab.desktop.editor.getAll .... ')
+% closeNoPrompt(matlab.desktop.editor.getAll);
+% disp('Closing all desktop editors matlab.desktop.editor.getAll .... DONE!')
 
+
+disp('Closing Simulink Test Manager .... ')
 sltest.testmanager.clearResults
 sltest.testmanager.close
+disp('Closing Simulink Test Manager .... DONE')
 
+disp('Closing Simulink Library Browser .... ')
 slLibraryBrowser
 slLibraryBrowser('close')   % Close Simulink Library Browser
+disp('Closing Simulink Library Browser .... DONE')
 
+disp('Closing Simulink Data Dictionaries .... ')
+Simulink.data.dictionary.closeAll('-discard')
+disp('Closing Simulink Data Dictionaries .... DONE')
+
+disp('Going back to project root folder .... ')
 proj = currentProject;
 cd(proj.RootFolder)
 
 clear proj
+disp('Going back to project root folder .... DONE')
 
 % Clear up MATLAB to start the next run
+disp('Close all figures .... ')
 close all                   % Close figures
+disp('Close all figures .... DONE')
+disp('Close all Block Diagrams (Simulink models) .... ')
 bdclose all                 % Close models
+disp('Close all Block Diagrams (Simulink models) .... DONE')
+disp('Close/Clear classes base using evalin(base,clear) .... ')
 evalin('base','clear')      % Clean up workspace
+disp('Close/Clear classes base using evalin(base,clear) .... DONE')
+
 clc                         % Clean up command window
 
 
